5974daef4803989f35e9a8bc0b55502ece2d4bc8
oreak <ore082@student.ubc.ca>
2025-06-05 15:29:31 -0700

    Nav link from BookPrewview -> associated Book Details Page now works and updated ample data

--- better-reads/frontend/src/components/Book/BookPreview.jsx
+++ better-reads/frontend/src/components/Book/BookPreview.jsx
@@ -29,7 +29,6 @@ export function BookPreview({isbn, coverUrl, title, rating, genres, isFavorite, 
                         d="M29.1667 39.9958L27.1125 38.1258C19.8167 31.51 15 27.1325 15 21.7917C15 17.4142 18.4283 14 22.7917 14C25.2567 14 27.6225 15.1475 29.1667 16.9467C30.7108 15.1475 33.0767 14 35.5417 14C39.905 14 43.3333 17.4142 43.3333 21.7917C43.3333 27.1325 38.5167 31.51 31.2208 38.1258L29.1667 39.9958Z"/>
                 </svg>
             </div>
-
             <div className="card-clickable" onClick={handleCardClick} role="button" tabIndex={0}>
                 <img src={coverUrl} alt={`${title} cover`} />
                 <div className="card-content">
