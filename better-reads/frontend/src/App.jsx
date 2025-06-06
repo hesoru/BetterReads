0ae79b26b57047c657548b2d447c245cb220829b
rex015 <rex015@student.ubc.ca>
2025-06-05 19:25:55 -0700

    change app and connected to router

--- better-reads/frontend/src/App.jsx
+++ better-reads/frontend/src/App.jsx
@@ -5,25 +5,30 @@ import BookDetailsPage from "./components/Book/BookDetailsPage.jsx";
 import UserProfile from "./pages/UserProfile";
 import SearchPage from "./pages/SearchPage";
 import Header from "./components/Home/Header";
+import Login from './components/Login/Login.jsx'
+import Signup from "./components/Signup/Signup.jsx";
 
 function App() {
     const firstBookIsbn = Object.keys(sampleData.books)[0];
     //console.log(firstBookIsbn)
   return (
+    <>
+    {/* <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f9fbfd' }}>
+      <Header userAvatar={sampleData.user.avatarUrl} />
+      <main style={{ flex: 1 }}></main>
+    </div> */}
     <BrowserRouter>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f9fbfd' }}>
-        <Header userAvatar={sampleData.user.avatarUrl} />
-        <main style={{ flex: 1 }}>
           <Routes>
-            <Route path="/" element={<SearchPage />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/search" element={<SearchPage />} />
             <Route path="/books/:isbn" element={
                 <BookDetailsPage />
             } />
             <Route path="/profile" element={<UserProfile />} />
           </Routes>
-        </main>
-      </div>
     </BrowserRouter>
+    </>
   )
 }
 
