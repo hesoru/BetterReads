17f864a2d10e3677475d5a9606736f506c037f5e
Helena <hesoru@students.cs.ubc.ca>
2025-06-05 19:54:56 -0700

    rerouting to remove header from login page

--- better-reads/frontend/src/App.jsx
+++ better-reads/frontend/src/App.jsx
@@ -1,30 +1,39 @@
 import sampleData from "./sampleData2.json";
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import BookDetailsPage from "./components/Book/BookDetailsPage.jsx";
 import UserProfile from "./pages/UserProfile";
 import SearchPage from "./pages/SearchPage";
 import Header from "./components/Home/Header";
+import LoginPage from "./pages/LoginPage";
 
-function App() {
-    const firstBookIsbn = Object.keys(sampleData.books)[0];
-    //console.log(firstBookIsbn)
+function AppContent() {
+  const location = useLocation();
+  const showHeader = location.pathname !== '/login';
+
   return (
-    <BrowserRouter>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f9fbfd' }}>
-        <Header userAvatar={sampleData.user.avatarUrl} />
-        <main style={{ flex: 1 }}>
-          <Routes>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f9fbfd' }}>
+      {showHeader && <Header userAvatar={sampleData.user.avatarUrl} />}
+      <main style={{ flex: 1 }}>
+        <Routes>
             <Route path="/" element={<SearchPage />} />
             <Route path="/books/:isbn" element={
                 <BookDetailsPage />
             } />
             <Route path="/profile" element={<UserProfile />} />
+            <Route path="/login" element={<LoginPage />} />
           </Routes>
         </main>
       </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
