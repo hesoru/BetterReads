64ec31e96217ff64161468c077304a4241e806fa
oreak <ore082@student.ubc.ca>
2025-07-04 20:41:40 -0700

    cleaned up

--- better-reads/backend/test/users.test.js
+++ better-reads/backend/test/users.test.js
@@ -44,40 +44,6 @@ let testUser2 = {
 const defaultAvatar =
     '../../src/images/icons/User_Profile_Image_NoLogo.png';
 
-// let mongo;
-
-// before(async () => {
-//   process.env.NODE_ENV = 'test'; //this is important to ensure not run on production database
-//   mongo = await MongoMemoryServer.create();
-//   await mongoose.connect(mongo.getUri());
-//   console.log("mongo.getUri()", mongo.getUri())
-// });
-
-// after(async () => {
-//   await mongoose.disconnect();
-//   await mongo.stop();
-// });
-
-// beforeEach(async () => {
-//   const { host, port } = mongoose.connection;
-//   const expectedHost = mongo.instanceInfo.ip;
-//   const expectedPort = mongo.instanceInfo.port;
-
-//   if (
-//     process.env.NODE_ENV !== 'test' ||
-//     host !== expectedHost ||
-//     port !== expectedPort
-//   ) {
-//     throw new Error(
-//       `Refusing to run deleteMany — connected to ${host}:${port}, expected ${expectedHost}:${expectedPort}`
-//     );
-//   }
-
-//   const collections = await mongoose.connection.db.collections();
-//   for (const coll of collections) {
-//     await coll.deleteMany();
-//   }
-// });
 
 describe('Users Tests', () => {
     it('GET /users returns all new users', async () => {
